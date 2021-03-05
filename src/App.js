786f182ecbf495509681c468c284f7576ae0449e
Justin Thareja <justin.thareja@gmail.com>
2021-03-05 16:22:00 -0500

    updates screen to change scroll

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
+import { useState } from 'react';
 import "./App.css";
 import TouchWheel from "./TouchWheel";
 import Controls from "./Controls";
 import Screen from "./Screen";
 
 function App() {
+  const [screenProps, setScreenProps] = useState({
+    type: "menu",
+    header: "iPod",
+    statusIcon: null,
+    selectedIndex: 0,
+    menuItems: [
+      { name: "Playlists", hasSubMenu: true },
+      { name: "Browse", hasSubMenu: true },
+      { name: "Extras", hasSubMenu: true },
+      { name: "Settings", hasSubMenu: true },
+      { name: "Backlight", hasSubMenu: false },
+    ],
+  });
+  
   function handleTick({ direction }) {
-    console.log("tick", direction);
+    const { selectedIndex, menuItems } = screenProps;
+    if (direction === "clockwise" && selectedIndex < menuItems.length - 1) {
+      setScreenProps({ ...screenProps, selectedIndex: selectedIndex + 1});
+    }
+
+    if (direction === "anticlockwise" && selectedIndex > 0) {
+      setScreenProps({ ...screenProps, selectedIndex: selectedIndex - 1 });
+    }
   }
 
   return (
     <div className="ipod">
-      <Screen />
+      <Screen {...screenProps}/>
       <Controls />
       <TouchWheel onTick={handleTick} />
     </div>
