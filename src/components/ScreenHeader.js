5d8eae13193b042dc459ba1b38de2ad9babfda54
Justin Thareja <justin.thareja@gmail.com>
2021-03-22 15:50:21 -0400

    adds proper playback progress

--- src/components/ScreenHeader.js
+++ src/components/ScreenHeader.js
@@ -1,9 +1,19 @@
 import BatteryIcon from "./BatteryIcon";
+import PlayIcon from "./PlayIcon";
+import PauseIcon from "./PauseIcon";
+import { useCurrentlyPlaying } from "../hooks/useCurrentlyPlaying";
 
-function ScreenHeader({ statusIcon, header }) {
+function ScreenHeader({ header }) {
+  const { data } = useCurrentlyPlaying();
   return (
     <div className="screen-header">
-      <div className="header-icon">{statusIcon}</div>
+      <div className="header-icon">
+        {data && data.body && data.body.is_playing ? (
+          <PlayIcon />
+        ) : (
+          <PauseIcon />
+        )}
+      </div>
       <div className="title truncate">{header}</div>
       <div className="header-icon">
         <BatteryIcon />
