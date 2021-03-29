434089c0e9f9951094e9b9e2eb3ba6c05594d500
Justin Thareja <justin.thareja@gmail.com>
2021-03-29 17:58:05 -0400

    updates usePlayer when spotify returns nothing

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,15 +1,22 @@
 import { useUser } from "../context/UserContext";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import spotifyApi from "../api/spotifyApi";
 import stub from "../stubs/player.json";
 
 function usePlayer() {
   const { user } = useUser();
+  const queryClient = useQueryClient();
   const query = useQuery(
     ["player"],
     () => spotifyApi.getMyCurrentPlaybackState(),
     {
       enabled: !!user,
+      onSuccess: async (data) => {
+        // Sometimes spotify returns without an item
+        if (data.statusCode === 200 && !data.body.item) {
+          await queryClient.invalidateQueries("player");
+        }
+      },
     }
   );
 
