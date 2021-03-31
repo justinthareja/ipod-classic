60bd9b695b59bc8df6e8cf4221b216df81bbc7cf
Justin Thareja <justin.thareja@gmail.com>
2021-03-31 14:56:45 -0400

    updates usePlayer to use react-query retry logic

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,23 +1,25 @@
 import { useUser } from "../context/UserContext";
-import { useQuery, useQueryClient } from "react-query";
+import { useQuery } from "react-query";
 import spotifyApi from "../api/spotifyApi";
 import stub from "../stubs/player.json";
 import { useNoop } from "../utils/helpers";
 
 function usePlayer() {
   const { user } = useUser();
-  const queryClient = useQueryClient();
   const query = useQuery(
     ["player"],
-    () => spotifyApi.getMyCurrentPlaybackState(),
+    async () => {
+      const data = await spotifyApi.getMyCurrentPlaybackState();
+      // Sometimes spotify returns without an item
+      // this will trigger react-query's retry behavior
+      if (data.statusCode === 200 && !data.body.item) {
+        throw new Error("Current playback state responded with invalid item");
+      }
+
+      return data;
+    },
     {
       enabled: !!user,
-      onSuccess: async (data) => {
-        // Sometimes spotify returns without an item
-        if (data.statusCode === 200 && !data.body.item) {
-          await queryClient.invalidateQueries("player");
-        }
-      },
     }
   );
   const fakeRemove = useNoop();
